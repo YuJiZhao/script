ce1663b4ccb799b7cd3d035658c5aa3d740fdd9f
eyes <792734338@qq.com>
2021-09-25 19:22:59 +0800

    demo

--- test/test.js
+++ test/test.js
@@ -1,250 +1,76 @@
-// ==UserScript==
-// @name         bilibili弹幕列表实时滚动播放
-// @namespace    https://eyesblog.gitee.io
-// @version      1.0
-// @description  在B站看视频时弹幕一多就会遮住内容，可我又想看视频又想看弹幕，就写了这个脚本，安装后右侧的弹幕列表会随视频播放而滚动，并且会有弹幕出现提示
-// @author       eyes++
-// @match        https://www.bilibili.com/video/*
-// @require      https://cdn.staticfile.org/jquery/3.5.0/jquery.min.js
-// @grant        none
-// ==/UserScript==
-
-(function () {
-    'use strict';
-    console.log("脚本执行了");
-    // 允许跨域请求，防止浏览器拦截
-    var meta = document.createElement("meta");
-    meta.httpEquiv = "Access-Control-Allow-Origin";
-    meta.content = "*";
-    $("head")[0].appendChild(meta);
-
-    // 初始化若干变量
-    var page_loc = location.href;
-    var page_start = page_loc.indexOf("?p=");
-    var page_p;
-
-    // 获取视频cid
-    if (page_start == -1 || page_start == 1) page_p = 0;
-    else page_p = page_loc.slice(page_start + 3) - 1;
-    var page_cid = window.__INITIAL_STATE__.videoData.pages[page_p].cid;
-
-    // 发起请求，拿到弹幕数据
-    $.ajax({
-        url: "https://api.bilibili.com/x/v1/dm/list.so?oid=" + page_cid,
-        type: "GET",
-        dataType: "XML",
-        success: function (xml) {
-            // 处理返回数据
-            console.log("成功获得数据：", xml);
-            data_deal(xml.all);
-        },
-        error: function (err) {
-            console.log(err);
-            alert("弹幕列表滚动脚本失效，可尝试更新最新版本脚本解决问题！\n如果有一定编程基础可以打开控制台查看报错信息。");
-        }
-    });
-
-    // 存储数据
-    function data_deal(data) {
-        var data_list = new Array(); // 存信息
-        var data_arr = new Array(); // 存内容
-        var data_length = data.length;
-        for (var i = 8; i < data_length; i++) {
-            var data_array = data[i].attributes.p.value.split(",");
-            $.each(data_array, function (i) {
-                data_array[i] = parseFloat(data_array[i]); // 转换类型，便于排序
-            })
-            data_list.push(data_array);
-            data_arr.push(data[i].innerHTML);
-        }
-
-        // 执行排序
-        data_sort(data_list, 0, data_list.length - 1, data_arr);
-
-        // 修改弹幕列表
-        $(document).mousemove(function() {
-            $(".bui-collapse-header").click(function () {
-                console.log("触发点击事件");
-                var clearInter = setInterval(function () {
-                    if ($(".player-auxiliary-danmaku-load-status").css("display") == "none") {
-                        list_change(data_list, data_arr);
-                        console.log("执行排序");
-                        clearInterval(clearInter);
-                    }
-                }, 100);
-            })
-            $(document).off("mousemove");
-            console.log("鼠标动了");
-        })
-    }
-
-    // 数据排序(快排算法)
-    function data_sort(data_list, l, r, data_arr) {
-        if (l < r) {
-            var i = l,
-                j = r,
-                temp = data_list[l],
-                t1, t2, t3;
-            while (i < j) {
-                while (i < j && data_list[j][0] >= temp[0]) j--;
-                while (i < j && data_list[i][0] <= temp[0]) i++;
-                if (i < j) {
-                    t1 = data_list[i];
-                    data_list[i] = data_list[j];
-                    data_list[j] = t1;
-                    t2 = data_arr[i];
-                    data_arr[i] = data_arr[j];
-                    data_arr[j] = t2;
+// 油猴脚本似乎无法是用加载完执行之类的函数，无奈之下我只好是用定时器监听页面加载情况
+let clearInter = setInterval(() => {
+    if ($(".bui-collapse-header")[0]) { // 监听列表是否加载完成
+        clearInterval(clearInter);
+        $(".bui-collapse-header").click(() => {
+            let clear = setInterval(() => {
+                if ($(".player-auxiliary-danmaku-load-status").css("display") == "none") { // 监听弹幕是否加载完成
+                    clearInterval(clear);
+                    // 排序
+                    $(".player-auxiliary-danmaku-btn-time")[0].click();
+                    // 滚动
+                    listScroll();
                 }
-            }
-
-            data_list[l] = data_list[i];
-            data_list[i] = temp;
-            t3 = data_arr[l];
-            data_arr[l] = data_arr[i];
-            data_arr[i] = t3;
-            data_sort(data_list, l, i - 1, data_arr);
-            data_sort(data_list, i + 1, r, data_arr);
-        }
-    }
-
-    // 时间格式转换(分转秒)
-    function time_m_s(a) {
-        if (a.length == 2) return a[0] * 60 + a[1];
-        else return a[0] * 3600 + a[1] * 60 + a[2];
-    }
-
-    // 时间格式转换(秒转分)
-    function time_s_m(a) {
-        a = Math.floor(a);
-        var m = Math.floor(a / 60);
-        var s = (a - Math.floor(a / 60) * 60);
-        if (m < 0) m = "00";
-        else if (m < 10) m = "0" + String(m);
-        if (s < 10) s = "0" + String(s);
-        return m + ":" + s;
-    }
-
-    // 发送日期格式转换
-    function data_s_d(a) {
-        var date = new Date(a * 1000);
-        var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-        var D = date.getDate();
-        if (D < 10) D = "0" + String(D) + ' ';
-        else D = D + ' ';
-        var h = date.getHours();
-        if (h < 10) h = "0" + String(h) + ":";
-        else if (h == 0) h = "00" + ":";
-        else h = h + ":";
-        var m = date.getMinutes();
-        if (m < 10) m = "0" + String(m);
-        else if (m == 0) m = "00";
-        return M + D + h + m;
-    }
-
-    // 获取当前视频播放秒数
-    function get_now_s() {
-        var time_now = $('.bilibili-player-video-time-now')[0].innerHTML.split(":");
-        $.each(time_now, function (i) {
-            time_now[i] = parseInt(time_now[i]);
-        })
-        return time_m_s(time_now);
-    }
-
-    function list_change(data_list, data_arr) {
-        // 修改页面控制
-        $(".player-auxiliary-danmaku-btn-time").removeAttr("orderby");
-        $(".player-auxiliary-danmaku-btn-danmaku").removeAttr("orderby");
-        $(".player-auxiliary-danmaku-btn-date").removeAttr("orderby");
-        $(".player-auxiliary-danmaku-function > .player-auxiliary-danmaku-btn-danmaku")[0].innerHTML = "滚动弹幕(共" + data_arr.length + "条)";
-
-        // 弹幕排序
-        list_sort(data_list, data_arr);
-    }
-
-    function list_sort(data_list, data_arr) {
-        // 破坏初始列表结构
-        $(".player-auxiliary-danmaku-wrap > div > ul").empty();
-        $(".player-auxiliary-danmaku-wrap > .player-auxiliary-danmaku-contaner").removeClass("player-auxiliary-danmaku-contaner player-auxiliary-bscrollbar");
-        $(".player-auxiliary-danmaku-wrap").off();
-        $(".player-auxiliary-danmaku-wrap > div > ul").off();
-
-        // 清除BFC
-        var div = document.createElement("div");
-        var nothing = document.createTextNode(".");
-        div.appendChild(nothing);
-        $(".player-auxiliary-danmaku-wrap > div > ul")[0].appendChild(div);
-        $(div).addClass("BFC-nothing");
-        $(".BFC-nothing").css("opacity", "0");
-
-        // 构造有序列表
-        $(data_arr).each(function (i, v) {
-            var li = document.createElement("li");
-            var span1 = document.createElement("span");
-            var span2 = document.createElement("span");
-            var span3 = document.createElement("span");
-            var span4 = document.createElement("span");
-            var text1 = document.createTextNode(time_s_m(data_list[i][0]));
-            var text2 = document.createTextNode(v);
-            var text3 = document.createTextNode(data_s_d(data_list[i][4]));
-            var text4 = document.createTextNode(">");
-            span1.appendChild(text1);
-            span2.appendChild(text2);
-            span3.appendChild(text3);
-            span4.appendChild(text4);
-            $(li).attr("dmno", i);
-            $(span2).attr("title", v);
-            $(li).addClass("danmaku-info-row");
-            $(span1).addClass("danmaku-info-time");
-            $(span2).addClass("danmaku-info-danmaku");
-            $(span3).addClass("danmaku-info-date");
-            $(span4).addClass("danmaku-info-animate");
-            li.appendChild(span1);
-            li.appendChild(span2);
-            li.appendChild(span3);
-            li.appendChild(span4);
-            $(".player-auxiliary-danmaku-wrap > div > ul")[0].appendChild(li);
-            $('.danmaku-info-animate').css({
-                'position': 'absolute',
-                'width': '15px',
-                'height': '100%',
-                'padding': '0',
-                'left': '0px',
-                'font-size': '15px',
-                'color': 'rgb(1,185,245)',
-                'transition': '1s ease-in-out'
-            });
+                console.log("弹幕加载定时器");
+            }, 100);
         });
-
-        // 播放动画(监听视频播放进度)
-        var isChange = $(".bilibili-player-video-time-now")[0].innerHTML;
-        player_animate(data_list, get_now_s());
-        setInterval(function () {
-            if (isChange != $(".bilibili-player-video-time-now")[0].innerHTML) {
-                player_animate(data_list); // 动画播放
-                isChange = $(".bilibili-player-video-time-now")[0].innerHTML;
-            }
-        }, 1000);
     }
+    console.log("列表加载定时器");
+}, 100)
 
-    // 播放动画
-    function player_animate(data_list) {
-        var time_now_s = get_now_s();
-        var length = data_list.length;
-        var begin = -1,
-            end = -1;
-        for (var i = 0; i < length; i++) {
-            if (Math.floor(data_list[i][0]) < time_now_s) continue;
-            if (Math.floor(data_list[i][0]) > time_now_s) break;
-            if (begin != -1) end++;
-            if (begin == -1) begin = end = i;
+// 执行脚本
+let listScroll = () => {
+    // 修改页面控制
+    $(".player-auxiliary-danmaku-btn-time").removeAttr("orderby");
+    $(".player-auxiliary-danmaku-btn-danmaku").removeAttr("orderby");
+    $(".player-auxiliary-danmaku-btn-date").removeAttr("orderby");
+
+    // 破环初始列表结构
+    let danmaku_num = $(".player-auxiliary-danmaku-btn-danmaku > span")[0].innerHTML;
+    $(".player-auxiliary-danmaku-function > .player-auxiliary-danmaku-btn-danmaku")[0].innerHTML = "正在滚动(共" + danmaku_num + "条)";  // 修改列表
+    $(".player-auxiliary-danmaku-btn-time > i").css("display", "none");  // 隐藏排序图标
+    $(".player-auxiliary-danmaku-wrap > .player-auxiliary-danmaku-contaner").removeClass("player-auxiliary-danmaku-contaner player-auxiliary-bscrollbar");  // 禁止滚轮事件
+
+    // 监听视频进度
+    videoListen();
+}
+
+// 实现监听视频进度
+let videoListen = () => {
+    var isChange = $(".bilibili-player-video-time-now")[0].innerHTML;
+    setInterval(function () {
+        if (isChange != $(".bilibili-player-video-time-now")[0].innerHTML) {
+            let now_s = get_now_s();
+            // 动画播放
+            animatePlayer(now_s);
+            // 弹幕滚动
+            listScroll(now_s);
+            isChange = $(".bilibili-player-video-time-now")[0].innerHTML;
         }
-        if (end == -1) return;
-        var li_height = parseFloat($(".player-auxiliary-danmaku-wrap > div > ul > li").css("height").replace("px", ""));
-        var li_width = parseFloat($(".player-auxiliary-danmaku-wrap > div > ul > li").css("width").replace("px", ""));
-        for (var i = begin; i <= end; i++) {
-            if (length > 15 && i > 11) $(".player-auxiliary-danmaku-wrap > div > ul > li:first").css("margin-top", "-" + (i - 11) * li_height + "px"); // 列表滚动
-            $($('.player-auxiliary-danmaku-wrap > div > ul > li')[i]).find(".danmaku-info-animate").css("left", li_width); // 播放动画
-        }
-    }
-})();
+    }, 1000);
+}
+
+// 实现滚动
+let listScroll = (now_s) => {
+
+}
+
+// 实现动画
+let animatePlayer = (now_s) => {
+
+}
+
+// 时间格式转换(分转秒)
+function time_m_s(a) {
+    if (a.length == 2) return a[0] * 60 + a[1];
+    else return a[0] * 3600 + a[1] * 60 + a[2];
+}
+
+// 获取当前视频播放秒数
+function get_now_s() {
+    var time_now = $('.bilibili-player-video-time-now')[0].innerHTML.split(":");
+    $.each(time_now, function (i) {
+        time_now[i] = parseInt(time_now[i]);
+    })
+    return time_m_s(time_now);
+}
